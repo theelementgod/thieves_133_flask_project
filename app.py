7a0624e71cbb27f694dd61fbf20ab13979e724b9
theelementgod <theelementgod@gmail.com>
2023-11-07 16:06:44 -0700

    small change

--- app.py
+++ app.py
@@ -11,24 +11,20 @@ def hello_trainer():
 @app.route('/pkmn_name', methods=['GET', 'POST'])
 def get_pkmn_data_name():
     if request.method == 'POST':
-        pkmn_name = request.form.get('pkmn_name')
-        print(pkmn_name)
+        pkmn_name = request.form.get('pkmn_name'.lower())
         pkmn_url = f'https://pokeapi.co/api/v2/pokemon/{pkmn_name}'
         pkmn_response = requests.get(pkmn_url)
         pkmn_data = pkmn_response.json()
-        print(pkmn_data['forms'][0]['name'])
         
         all_pkmn = get_pkmn_data(pkmn_data)
-        print(all_pkmn)
         return render_template('pkmn_name.html', all_pkmn=all_pkmn)
     else:
         return render_template('pkmn_name.html')
 
 def get_pkmn_data(pkmn_data):
-    print(pkmn_data)
     new_pkmn_data =[]
     pkmn_dict = {
-        'pkmn_name': pkmn_data['forms'][0]['name'],
+        'pkmn_name': (pkmn_data['forms'][0]['name']),
         'ability': pkmn_data['abilities'][0]['ability']['name'],
         'hp': pkmn_data['stats'][0]['base_stat'],
         'attack': pkmn_data['stats'][1]['base_stat'],
